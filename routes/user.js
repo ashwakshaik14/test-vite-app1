625d79fce3b34812283bf3d97c8113da0b517278
Shaik Mohammad Ashwak <80761480+ashwakshaik14@users.noreply.github.com>
2025-01-03 23:00:15 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -154,7 +154,7 @@
 //     res.status(201).json({ message: "User created successfully" });
 //   } catch (err) {
 //     console.error("Error in /register route:", err);
-//     res.status(500).json({ message: "Internal Server Error" });
+//     res.status(500).json({ message: err });
 //   }
 // });
 
